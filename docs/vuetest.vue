b9cce3dc059eb5f53c61631d6340fe93f7ffeb12
Elliot Knight <13136649471@163.com>
2022-08-07 21:36:33 +0800

    feat: 使用Vue组件

---
+++ docs/vuetest.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="text">
+        这里是Vue组件渲染出的文本
+    </div>
+</template>
+
+<script setup ></script>
+
+<style scoped>
+.text {
+    background: -webkit-linear-gradient(120deg, #bd34fe, #41d1ff);
+}
+</style>
